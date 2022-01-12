e538ae8ab6407c8a3ad7609725e83251cd13eabf
Akhireddy <akhir1989@gmail.com>
2022-01-13 00:00:29 +0800

    added first file added

--- 03-resources/resource.tf
+++ 03-resources/resource.tf
@@ -1,8 +1,8 @@
 resource "aws_instance" "sample" {
-  ami = "ami-083602cee93914c0c"
+  ami           = "ami-083602cee93914c0c"
   instance_type = "t2.micro"
 }
 
 output "out" {
-  value= aws_instance.sample
+  value = aws_instance.sample
 }
