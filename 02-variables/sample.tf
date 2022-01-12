6bea5a770496eea14280e030998c602560b0c267
Akhireddy <akhir1989@gmail.com>
2022-01-12 17:16:28 +0800

    added first file added

--- 02-variables/sample.tf
+++ 02-variables/sample.tf
@@ -4,3 +4,46 @@ variable "demo" {
 output "demo" {
   value = var.demo
 }
+
+variable "demo" {
+  type    = string
+  default = "Hello World"
+}
+
+output "demo" {
+  value = var.demo
+}
+
+variable "demo1" {
+  default = 100
+  type    = number
+}
+
+variable "demo2" {
+  description = "Demo variable for bool demo"
+  default     = true
+  type        = bool
+}
+
+
+variable "demo3" {
+  description = "List Variable"
+  default     = [100, "abc"]
+}
+
+variable "demo4" {
+  description = "Map Variable"
+  default = {
+    course     = "DevOps"
+    timing     = "6am"
+    is_started = true
+  }
+}
+
+output "demo1" {
+  value = var.demo3[0]
+}
+
+output "demo2" {
+  value = var.demo4["course"]
+}
